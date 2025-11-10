a4a3b9eaa48e5c5e36489f24342b48bb37a2c296
amberrosana <amberprincessrosana05@gmail.com>
2025-11-10 15:26:03 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  
+  resolve: {
+    preserveSymlinks: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
